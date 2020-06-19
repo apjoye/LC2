cdffd903868f08f45d2da1a73e046e512303cf94
Vishesh Kumar <vishesh@visheshk.net>
2020-06-19 17:19:39 -0400

    adding google fonts attempt

---
+++ imports/load-font.js
@@ -0,0 +1,13 @@
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: [
+      // 'Nunito:regular,bold',
+      // 'Lato:regular,bold,italic',
+      // 'Londrina Solid: light,regular, bold'
+      'Solway: light, regular, medium'
+    ],
+  },
+});
+
